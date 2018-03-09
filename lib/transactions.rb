d6e7956f3f72bc1664515818e539cfcca06ea752
Carl Thuringer <carlthuringer@gmail.com>
2018-03-09 16:52:48 -0600

    [feat ] Use PG gem and dotenv to pull data from postgres

--- lib/transactions.rb
+++ lib/transactions.rb
@@ -1,10 +1,16 @@
 # frozen_string_literal: true
 
 require 'csv'
+require 'pg'
 
 # Imports transactions
 class Transactions
   attr_reader :table, :set
+
+  def initialize(conn = default_connection)
+    @conn = conn
+  end
+
   def import
     path = File.expand_path(
       File.join(File.dirname(__FILE__), '..', 'data', 'transactions.csv')
@@ -12,7 +18,22 @@ class Transactions
     @table = CSV.table(path)
   end
 
+  def load
+    query = <<~SQL
+      SELECT id
+      FROM transactions
+      WHERE created_at > $1
+    SQL
+    @table = @conn.exec(query, [ENV['START_AT']])
+  end
+
   def build_set
-    @set = @table.map { |r| r[:id] }.to_set
+    @set = @table.map { |r| r['id'] }.to_set
+  end
+
+  private
+
+  def default_connection
+    PG.connect(ENV['CORE_SERVICE_DB_URI'])
   end
 end
