21ab8ee1cd7f26caf535bc94368497e609c22c0d
Carl Thuringer <carlthuringer@gmail.com>
2018-03-12 14:48:24 -0500

    [feat ] And be able to reconcile loyalty events

--- lib/events.rb
+++ lib/events.rb
@@ -6,26 +6,37 @@ require 'pg'
 
 # Imports and processes events
 class Events
-  attr_reader :table, :set
+  attr_reader :transaction_events,
+              :transactions_set,
+              :loyalty_events,
+              :loyalty_events_set
   def initialize(conn = default_connection)
     @conn = conn
   end
 
-  def load
+  def load_transactions
     query = <<~SQL
       SELECT id, context
       FROM events
       WHERE created_at > $1
       AND event_type = 'transactions'
     SQL
-    @table = @conn.exec(query, [ENV['START_AT']])
+    @transaction_events = @conn.exec(query, [ENV['START_AT']])
   end
 
-  def build_set
-    @set = table.map do |row|
-      context = JSON.parse(
-        JSON.parse(row['context']), symbolize_names: true
-      )
+  def load_loyalty_events
+    query = <<~SQL
+      SELECT id, context
+      FROM events
+      WHERE created_at > $1
+      AND event_type in ('profile_completion', 'wheel_spin', 'additional_questions')
+    SQL
+    @loyalty_events = @conn.exec(query, [ENV['START_AT']])
+  end
+
+  def build_transactions_set
+    @transactions_set = transaction_events.map do |row|
+      context = parse_context(row['context'])
       transaction = context.find do |resource|
         resource[:type] == 'transactions'
       end
@@ -34,9 +45,25 @@ class Events
     end.to_set
   end
 
+  def build_loyalty_events_set
+    @loyalty_events_set = loyalty_events.map do |row|
+      context = parse_context(row['context'])
+      loyalty_event = context.find do |resource|
+        resource[:type] == 'loyalty_events'
+      end
+      loyalty_event[:id]
+    end
+  end
+
   private
 
   def default_connection
     PG.connect(ENV['RULE_SERVICE_DB_URI'])
+  end
+
+  def parse_context(context)
+    JSON.parse(
+      JSON.parse(context), symbolize_names: true
+    )
   end
 end
