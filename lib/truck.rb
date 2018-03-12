df78eb20798aac8e24d253fc1d1fb2ef5d10f6b0
Carl Thuringer <carlthuringer@gmail.com>
2018-03-12 09:24:26 -0500

    Add some additional query to add data to the output

--- lib/truck.rb
+++ lib/truck.rb
@@ -3,5 +3,28 @@
 require 'truck/version'
 
 module Truck
-  # Your code goes here...
+  # Box to put the run instructions in
+  class Runner
+    def run
+      txs = Transactions.new
+      txs.load
+      txs.build_set
+
+      evs = Events.new
+      evs.load
+      evs.build_set
+
+      unmatched_ts = txs.set - evs.set
+      transaction_data = txs.find(unmatched_ts)
+      write(transaction_data.map { |row| [row['id'], row['created_at']] })
+    end
+
+    def write(data)
+      CSV.open('out.csv', 'w') do |csv|
+        data.each do |datum|
+          csv << datum
+        end
+      end
+    end
+  end
 end
