0dfa080ee9f189479f9513e8b927a26dabbf1b58
Carl Thuringer <carlthuringer@gmail.com>
2018-03-12 12:51:39 -0500

    [feat ] More rakeable API for truck runner

--- lib/truck.rb
+++ lib/truck.rb
@@ -5,18 +5,30 @@ require 'truck/version'
 module Truck
   # Box to put the run instructions in
   class Runner
+    attr_reader :transactions, :events, :unmatched_transactions
+    def initialize
+      @transactions = Transactions.new
+      @events = Events.new
+    end
+
+    def collect_unmatched_transactions
+      transactions.load
+      transactions.build_set
+
+      events.load
+      events.build_set
+
+      @unmatched_ts = transactions.set - events.set
+    end
+
+    def fetch_transaction_data(transaction_ids)
+      transactions.find(transaction_ids)
+    end
+
     def run
-      txs = Transactions.new
-      txs.load
-      txs.build_set
-
-      evs = Events.new
-      evs.load
-      evs.build_set
-
-      unmatched_ts = txs.set - evs.set
-      return print "No missing transactions" unless unmatched_ts
-      transaction_data = txs.find(unmatched_ts)
+      collect_unmatched_transactions
+      return print 'No missing transactions' if unmatched_ts.empty?
+      transaction_data = transactions.find(unmatched_ts)
       write(transaction_data.map { |row| [row['id'], row['created_at']] })
     end
 
